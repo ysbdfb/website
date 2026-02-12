71d3b3776a8ec2e3dfd56c1d58ed37ca107de733
andersondenis927-afk <andersondenis927@gmail.com>
2026-02-12 13:44:49 +0200

    ..

--- footer.php
+++ footer.php
@@ -6,19 +6,18 @@
         <div>
             <h3>About us</h3>
             <ul>
-                <li>Home</li>
-                <li>Recipes</li>
-                <li>Share your poison</li>
-                <li>Witch account</li>
+                <li><a href="main.html">Home</a></li>
+                <li><a href="recipes.html">Recipes</a></li>
+                <li><a href="poisonrecipe.html">Share your poison</a></li>
+                <li><a href="account.html">Witch account</a></li>
             </ul>
         </div>
         <div>
             <h3>Contact us</h3>
             <ul>
-                <li><a href="main.html">Home</a></li>
-                <li><a href="recipes.html">Recipes</a></li>
-                <li><a href="poisonrecipe.html">Share your poison</a></li>
-                <li><a href="account.html">Witch account</a></li>
+                <li>denis.anderson@student.hamk.fi</li>
+                <li>martin.pevgonen@student.hamk.fi</li>
+                <li>jekaterina.zasijenko@student.hamk.fi</li>
             </ul>
         </div>
     </div>
