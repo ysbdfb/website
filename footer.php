f92270cbc8e4407c762a9b0205782fadc43083d0
andersondenis927-afk <andersondenis927@gmail.com>
2026-02-12 13:44:31 +0200

    ..

--- footer.php
+++ footer.php
@@ -15,9 +15,10 @@
         <div>
             <h3>Contact us</h3>
             <ul>
-                <li>denis.anderson@student.hamk.fi</li>
-                <li>martin.pevgonen@student.hamk.fi</li>
-                <li>jekaterina.zasijenko@student.hamk.fi</li>
+                <li><a href="main.html">Home</a></li>
+                <li><a href="recipes.html">Recipes</a></li>
+                <li><a href="poisonrecipe.html">Share your poison</a></li>
+                <li><a href="account.html">Witch account</a></li>
             </ul>
         </div>
     </div>
