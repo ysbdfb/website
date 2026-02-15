19a542c834c56b575191960ae72601a00981a953
DenisAnderson981 <amk1011125@student.hamk.fi>
2026-02-15 21:44:14 +0200

    ja ustal

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer>
+    <div class="blocks container">
+        <div>
+            <span class="lastmeal">LAST MEAL</span>
+        </div>
+        <div>
+            <h3>About us</h3>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="recipes.php">Recipes</a></li>
+                <li><a href="poisonrecipe.php">Share your poison</a></li>
+                <li><a href="account.php">Witch account</a></li>
+            </ul>
+        </div>
+        <div>
+            <h3>Contact us</h3>
+            <ul>
+                <li>denis.anderson@student.hamk.fi</li>
+                <li>martin.pevgonen@student.hamk.fi</li>
+                <li>jekaterina.zasijenko@student.hamk.fi</li>
+            </ul>
+        </div>
+    </div>
+</footer>
